4c18c4305880b541fe6904865f80f94761c72e9c
MyNameIsDeepak25 <deepakkhedkar31@gmail.com>
2026-01-31 15:58:10 +0530

    initial commit

---
+++ demo/src/main/java/com/infy/ems/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.infy.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.infy.ems.entity.Employee;
+
+public interface EmployeeRepository  extends JpaRepository<Employee, Long>{
+
+}
